9097c8aa64ad9aa23c0a02ba1330dfa7ec075d0a
= <=>
2024-11-14 23:15:29 +0530

    layout-updates

--- components/AboutHack.tsx
+++ components/AboutHack.tsx
@@ -14,7 +14,7 @@ export default function AboutHack() {
     <Parallax speed={-10}>
       <div className="w-11/12 m-auto bg-pattern h-[90vh] items-center rounded-2xl">
         <div className="w-full h-full bg-black bg-opacity-50 flex justify-center items-center p-20">
-          <p className="font-semibold text-2xl">
+          <p className="font-semibold text-2xl text-white">
             HackTheSpace is an exciting 24-hour event that takes place in
             Bhilai, India. It is a time-limited event that encourages
             participants to brainstorm, develop, and present their ideas to a
--- components/Numbers.tsx
+++ components/Numbers.tsx
@@ -5,7 +5,7 @@ import { Parallax } from "react-scroll-parallax";
 export default function Numbers(){
     return <div className="w-full h-[60vh] flex justify-evenly items-center">
       <Parallax speed={5}>
-    <div className="w-60 h-60 p-5 flex justify-center items-center border-4 border-amber-700">
+    <div className="w-60 h-60 p-5 flex justify-center items-center border-amber-700">
       <div className="felx flex-col justify-center hover:border-b-8 hover:border-amber-400 text-6xl font-extrabold duration-200">
         <p className="text-center text-white">12+</p>
         <p className="text-center text-orange-600">Theme</p>
@@ -14,7 +14,7 @@ export default function Numbers(){
     </div>
     </Parallax>
     <Parallax speed={-5}>
-    <div className="w-60 h-60 p-5 flex justify-center items-center border-4 border-amber-700">
+    <div className="w-60 h-60 p-5 flex justify-center items-center border-amber-700">
       <div className="felx flex-col justify-center hover:border-b-8 hover:border-amber-400 text-6xl font-extrabold duration-300">
         <p className="text-center text-white">24+</p>
         <p className="text-center text-orange-600">Hours</p>
@@ -23,7 +23,7 @@ export default function Numbers(){
     </div>
     </Parallax>
     <Parallax speed={5}>
-    <div className="w-60 h-60 p-5 flex justify-center items-center border-4 border-amber-700">
+    <div className="w-60 h-60 p-5 flex justify-center items-center border-amber-700">
       <div className="felx flex-col justify-center hover:border-b-8 hover:border-amber-400 text-6xl font-extrabold duration-200">
         <p className="text-center text-white">150+</p>
         <p className="text-center text-orange-600">Projects</p>
